73fbff5bf2d3d85d72a8c4f5666655a6202340f9
tanuj <tanuj@SPARTAN>
2025-05-11 23:20:57 -0400

    Code Changes done

---
+++ HrInternalTool.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HrInternalTool.Api.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _requestDelegate = next;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try {
+            await _requestDelegate(context);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception,exception.Message);
+                var problemDetails = new ProblemDetails
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Title = "Server Error"
+                };
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    await context.Response.WriteAsJsonAsync(problemDetails);
+            }
+        }
+    }
+}
